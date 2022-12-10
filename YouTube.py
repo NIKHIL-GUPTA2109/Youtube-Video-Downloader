13610eac07a259290cc4f07e67decadc53d922a5
NIKHIL-GUPTA2109 <94130774+NIKHIL-GUPTA2109@users.noreply.github.com>
2022-12-10 18:07:28 +0530

    Add files via upload

---
+++ YouTube.py
@@ -0,0 +1,10 @@
+from pytube import YouTube
+print("--------------------------------x--------------------------------")
+print("YouTube Video Downloader")
+print("--------------------------------x--------------------------------")
+print("Download YouTube videos in High Quality!")
+print("--------------------------------x--------------------------------")
+link= input("Enter your link here: ")
+yt = YouTube(link)
+yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download()
+print("Task Performed!")
